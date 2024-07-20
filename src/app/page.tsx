c49061963b2ebcf0684c763a2305679ba5b75c78
Popri01 <lucas.popri@gmail.com>
2024-07-20 15:18:55 -0300

    Agregar Participantes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 
 import {motion} from "framer-motion";
-import {useState} from "react";
+import {useState, useRef} from "react";
 
 import {Button} from "@/components/ui/button";
-
-const Personas: Opcion[] = [
-  {id: 0, nombre: "Martin"},
-  {id: 1, nombre: "Ana"},
-  {id: 2, nombre: "Luis"},
-  {id: 3, nombre: "Carlos"},
-  {id: 4, nombre: "Maria"},
-  {id: 5, nombre: "Sofia"},
-  {id: 6, nombre: "Jorge"},
-  {id: 7, nombre: "Laura"},
-  {id: 8, nombre: "Pedro"},
-  {id: 9, nombre: "Elena"},
-];
-
+import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
 const colors = [
   "bg-red-500",
   "bg-green-500",
@@ -37,42 +25,52 @@ interface Opcion {
 }
 
 export default function HomePage() {
-  const ValorInicial: Opcion = Personas[0];
-  // const [x, setX] = useState(ValorInicial);
-  // const [y, setY] = useState(ValorInicial);
+  const [personas, setPersonas] = useState<Opcion[]>([
+    {id: 0, nombre: "Martin"},
+    {id: 1, nombre: "Ana"},
+    {id: 2, nombre: "Luis"},
+    {id: 3, nombre: "Carlos"},
+    {id: 4, nombre: "Maria"},
+    {id: 5, nombre: "Sofia"},
+    {id: 6, nombre: "Jorge"},
+    {id: 7, nombre: "Laura"},
+    {id: 8, nombre: "Pedro"},
+    {id: 9, nombre: "Elena"},
+  ]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const ValorInicial: Opcion = personas[0];
   const [ganador, setGanador] = useState<Opcion>(ValorInicial);
-  // const MoverDerecha = () => {
-  //   setX((x) => x + 100);
-  // };
 
-  // const MoverIz = () => {
-  //   setY((y) => y + 100);
-  // };
-
-  // const ReiniciarX = () => {
-  //   setX(ValorInicial);
-  // };
-
-  // const ReiniciarY = () => {
-  //   setY(ValorInicial);
-  // };
   const ElegirGanador = () => {
-    const randomId = Math.floor(Math.random() * Personas.length);
-    const ganador: Opcion | undefined = Personas.find((persona) => persona.id == randomId);
+    const randomId = Math.floor(Math.random() * personas.length);
+    const ganador: Opcion | undefined = personas.find((persona) => persona.id === randomId);
 
     if (!ganador) return;
     setGanador(ganador);
   };
 
+  const AgregarPersona = () => {
+    if (inputRef.current !== null) {
+      const nuevaPersona: Opcion = {
+        id: personas.length + 1,
+        nombre: inputRef.current.value,
+      };
+
+      setPersonas((prevPersonas) => [...prevPersonas, nuevaPersona]);
+      inputRef.current.value = "";
+    }
+  };
+
   return (
     <div className="flex flex-col items-center space-y-6 p-5">
       <div className="grid grid-cols-5 gap-6">
-        {Personas.map((persona, index) => (
+        {personas.map((persona, index) => (
           <motion.div
             key={persona.id}
             className={`flex h-16 w-16 items-center justify-center text-white shadow-xl ${
               colors[index % colors.length]
-            } ${persona.nombre === ganador.nombre ? "shadow-cyan-500/50" : ""}`}
+            } ${persona.id === ganador.id ? "shadow-cyan-500/50" : ""}`}
           >
             {persona.nombre}
           </motion.div>
@@ -85,6 +83,12 @@ export default function HomePage() {
       >
         Girar
       </button>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input ref={inputRef} placeholder="Ingrese un nombre" />
+        <Button type="submit" onClick={AgregarPersona}>
+          Agregar
+        </Button>
+      </div>
       <p className="mt-4 text-lg font-bold">Ganador: {ganador.nombre}</p>
     </div>
   );
